5b0a30a2e5a411dafff0288202e46f21740fd8d6
iamadal <mdadalkhan@gmail.com>
2021-07-07 13:34:04 +0600

    Wed 07/07/2021

---
+++ js/api.js
@@ -0,0 +1,13 @@
+/*
+ * Green University Management System - Core JavaScript API
+ */
+
+'use strict'
+
+const io = function(){
+    module = {
+      const MAXSIZE:1000,
+      bcrypt = /?[4/334?/
+    
+    }
+}
