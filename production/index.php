1cff0ede9aff07576c24f1640f54cf112c70284c
iamadal <mdadalkhan@gmail.com>
2021-07-20 10:55:14 +0600

    new files for login are added

--- production/index.php
+++ production/index.php
@@ -1,2 +1,22 @@
 <?php
-  echo("Sample PHP Code");
+ /*
+  * 01. mapping 
+  *
+  * /admin
+  * /student
+  * /teacher
+  * 
+  * 
+  * 
+  * 
+  * /signup-admin      <--->  /login-admin
+  * /signup-students   <--->  /login-students
+  * /signup-teacher    <--->  /login-teacher
+  * 
+  *
+  *
+  *
+  */
+
+
+ 
