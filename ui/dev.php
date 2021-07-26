0a35215dfba81b1bf1188e432c3e94763b4a1e92
iamadal <mdadalkhan@gmail.com>
2021-07-26 21:51:13 +0600

    Application Integration is started

---
+++ ui/dev.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="views/css/normalize.min.css">
+	<link rel="shortcut icon" type="image/jpg" href="img/fav.png">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Open+Sans:wght@300;400&family=Roboto:wght@100;300;400&family=Ubuntu:wght@300;400&display=swap" rel="stylesheet">
+	<title>Green University of Bangladesh</title>
+</head>
+<body>
+   <!-- Header section -->
+   <div class="header">Header</div>
+
+   <!-- Navigation System -->
+   <div class="navbar">Navbar</div>
+   
+   <!-- main content area -->
+   <div class="content">Content</div>
+   
+   <!-- footer section -->
+   <div class="footer">footer</div>
+</body>
+
+</html>
+
+<!-- Stylesheets -->
+<style>
+div       {border: 1px solid red;}
+body,html {font-family: 'ubuntu'; font-weight: 300}
+
+/* handle very small devices < 320px*/
+
+
+
+
+/* media queries*/
+/* Very Small devices*/
+@media (min-width: 320px){
+
+}
+/*Smart Phones*/
+@media (min-width: 481px){
+
+}
+/* Table */
+@media (min-width: 768px){
+
+}
+/*Desktop*/
+@media (min-width: 1364px){
+	
+}
+
+</style>
+<!-- Script -->
+<script>
+	
+</script>
